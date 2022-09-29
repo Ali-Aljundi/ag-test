41489c3f22a4a4731ec957e5866910d4e736114e
alialjonde24 <alialjonde24@gmail.com>
2022-09-30 01:22:02 +0300

    update responsive

--- algooru/src/app/app.component.ts
+++ algooru/src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'algooru';
+
+  public cards = [
+    {
+      'author':'Khaled Abdul',
+      'date':'Feb 2, 2022',
+      'content':'This was the easiest way to get a tutor. I want to personally thank Mr. Ali Abdulrahman for really helping me ace my AP Biology exams',
+      'logo':'Google play',
+    },
+    {
+      'author':'Khaled Abdul',
+      'date':'Feb 2, 2022',
+      'content':'This was the easiest way to get a tutor. I want to personally thank Mr. Ali Abdulrahman for really helping me ace my AP Biology exams',
+      'logo':'Vector (5)',
+    }
+  ]
 }
